02254a3059db60b41197f2cccddece956f04620a
Cumar Cumar <umar-19@live.com>
2022-05-25 17:48:17 -0500

    worked on some logic affecting the databases

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -17,12 +19,74 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "text editor",
+        start_url: '/',
+        publicPath: '/',
+        background_color: '#272822',
+        theme_color: '#272822',
+
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join("assets", "icons"),
+          },
+        ],
+
+      }),
+      new WorkboxPlugin.GenerateSW({
+        exclude: [/\.(?:png|jpg|jpeg|svg|)$/],
+        runtimeCaching: [{
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName:'images',
+            expiration: {
+              maxEntries: 2,
+            }
+          }
+        }]
+      })
     ],
 
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+    
         
       ],
     },
